cd15321f3fe339b681f33eb018a3112e5977419b
slavar <m3atyy@gmail.com>
2024-03-18 15:22:46 +0500

    fix: add milliseconds to dates

--- src/dto.ts
+++ src/dto.ts
@@ -5,6 +5,8 @@ import type {
 	TickerAssetResponse,
 } from "./types/Ticker";
 
+const MILLISECONDS = 1000;
+
 export function NewTickerPriceDTO(data: TickerPriceResponse): _NewTickerPriceDTO {
 	return {
 		symbol: data?.FROMSYMBOL ?? "",
@@ -20,10 +22,10 @@ export function TickerAssetDTO(data: TickerAssetResponse): _TickerAssetDTO {
 		logoUrl: data?.Data.LOGO_URL ?? "",
 		description: data?.Data.ASSET_DESCRIPTION_SNIPPET ?? "",
 		websiteUrl: data?.Data.WEBSITE_URL ?? "",
-		createdOn: data?.Data.CREATED_ON ? new Date(data.Data.CREATED_ON) : 0,
-		launchDate: data?.Data.LAUNCH_DATE ? new Date(data.Data.LAUNCH_DATE) : 0,
+		createdOn: data?.Data.CREATED_ON ? new Date(data.Data.CREATED_ON * MILLISECONDS) : 0,
+		launchDate: data?.Data.LAUNCH_DATE ? new Date(data.Data.LAUNCH_DATE * MILLISECONDS) : 0,
 		priceInUSD: data?.Data.PRICE_USD ?? 0,
-		priceInUSDUpdatedOn: data?.Data.PRICE_USD_LAST_UPDATE_TS ? new Date (data?.Data.PRICE_USD_LAST_UPDATE_TS) : 0,
+		priceInUSDUpdatedOn: data?.Data.PRICE_USD_LAST_UPDATE_TS ? new Date (data?.Data.PRICE_USD_LAST_UPDATE_TS * MILLISECONDS) : 0,
 		fullDescription: data?.Data.ASSET_DESCRIPTION ?? "",
 	};
 }
